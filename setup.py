cab3d639b78f625691d0342f94371a5cc629b371
Wentao Han <wentao.han@gmail.com>
2020-08-04 20:39:49 +0800

    Add support to Python 3.8

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='html table csv convert',
     packages=find_packages(exclude=['tests']),
