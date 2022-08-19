630331fae8fed26cacfb28b556a10a5a86030d18
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-08-19 10:43:17 -0400

    Added `convert_to_list` as intermediate function

--- html2csv/converter.py
+++ html2csv/converter.py
@@ -24,15 +24,29 @@ class Converter:
             self.engine = engine
         self.params = kwargs
 
-    def convert(self, html_doc):
+    def convert_to_list(self, html_doc):
         soup = bs4.BeautifulSoup(html_doc, self.engine)
+        tables = []
+        for table in soup.find_all('table'):
+            table = []
+            for tr in table.find_all('tr'):
+                row = [''.join(cell.stripped_strings)
+                       for cell in tr.find_all(['td', 'th'])]
+                table.append(row)
+            tables.append(table)
+        return tables
+
+    def convert_to_csv(self, html_doc):
+        tables = self.convert_to_list(html_doc)
         output = []
-        for table_num, table in enumerate(soup.find_all('table')):
+        for table_num, table in enumerate(tables):
             csv_string = io.StringIO()
             csv_writer = csv.writer(csv_string)
-            for tr in table.find_all('tr'):
-                row = [''.join(cell.stripped_strings) for cell in tr.find_all(['td', 'th'])]
+            for row in table:
                 csv_writer.writerow(row)
             table_attrs = dict(num=table_num)
             output.append((csv_string.getvalue(), table_attrs))
         return output
+
+    def convert(self, html_doc):
+        return self.convert_to_csv(html_doc)
